8bf0dab153a7669efd4580372cb47ae2c953ff08
vinojiniparamasivam <Vinojini76@gmail.com>
2021-01-02 10:02:19 +0530

    Add chrome elements to the web site

---
+++ gfg.php
@@ -0,0 +1,68 @@
+<html>
+<body>
+<?php 
+
+	
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+				
+	function get_data() { 
+		$name = $_POST['name']; 
+		$file_name='POICommentData'. '.json'; 
+$current_date = date("M d, Y");
+		if(file_exists("$file_name")) { 
+			$current_data=file_get_contents("$file_name"); 
+			$array_data=json_decode($current_data, true); 
+							
+			$extra=array( 
+				'Name' => $_POST['name'],
+				'Date' => $current_date,
+				//new DateTime($content['date']),
+				'Comment' => $_POST['comment'], 
+				'ReplyComment' => '',
+							); 
+			$array_data[]=$extra;  
+			return json_encode($array_data); 
+		} 
+		else { 
+			$datae=array(); 
+			$datae[]=array( 
+				'Name' => $_POST['name'], 
+				'Date' => $current_date,
+				//new DateTime($content['date']),
+				'Comment' => $_POST['comment'], 
+				'ReplyComment' => '',
+			); 
+			return json_encode($datae); 
+		} 
+	} 
+
+	$file_name='POICommentData'. '.json'; 
+	
+	if(file_put_contents("$file_name", get_data())) {
+//echo '<script>alert("Success")</script>';
+//echo '<script>window.location = "file:///C:/Users/user/Desktop/UIUX/CWK-2/Student%202/Offers.html"</script>';
+	//header('Location:index2.php');
+	
+    echo "<script>
+alert('Your comment has been added. Thank you for your feedback');
+window.location.href='index2.php';
+</script>";
+	
+
+		 
+	}				 
+	else { 
+	//echo '<script>alert("Error Occurred Please Try Again")</script>';	
+		//echo 'There is some error';	
+echo "<script>
+alert('Error Occurred Please Try Again');
+window.location.href='index1.html';
+</script>";		
+	} 
+} 
+	
+?>
+
+</body> 
+</html>
+
---
+++ index2.php
@@ -0,0 +1,161 @@
+<?php
+
+$filename = 'POICommentData.json';
+
+$data = file_get_contents($filename);
+$users = json_decode($data);
+?>
+
+<html>
+<head
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+	<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
+	
+	<style> 
+		table { 
+			margin: 0 auto; 
+			font-size: large; 
+			border: 1px solid black; 
+		}
+h1 { 
+			text-align: center; 
+			color: #006600; 
+			font-size: xx-large; 
+			font-family: 'Gill Sans', 
+				'Gill Sans MT', ' Calibri', 
+				'Trebuchet MS', 'sans-serif'; 
+		} 
+
+		td { 
+			background-color: #E4F5D4; 
+			border: 1px solid black; 
+		} 
+
+		th, 
+		td { 
+			font-weight: bold; 
+			border: 1px solid black; 
+			padding: 10px; 
+			text-align: center; 
+		} 
+
+		td { 
+			font-weight: lighter; 
+		} 
+		
+		.fa {
+font-size: 10px;
+cursor: pointer;
+user-select: none;
+}
+.fa-thumbs-up {
+color: blue;
+}
+.fa-thumbs-down {
+color: red;
+}
+.fa:hover {
+transform: scale(1.2);
+}
+
+
+
+
+.start-button {
+  color: #eee;
+  background-color: #4485F3;
+  width: 250px;
+  height: 84px;
+  margin: 60px;
+  border-radius: 20px;
+  font-weight: bold;
+  font-size: 24px;
+  box-shadow: 2px 2px #111;
+}
+
+form {
+    width: 100%;
+
+    .like-button {
+        width: 250px;
+        height: 84px;
+        background-color: #eee;
+        margin: 60px;
+        border-radius: 20px;
+        font-weight: bold;
+        font-size: 24px;
+        box-shadow: 2px 2px #111;
+    }
+}
+
+
+
+
+		
+	</style>
+	<script>
+	function setColor(e, btn, color) {
+  var target = e.target,
+      count = +target.dataset.count;
+  
+   target.style.backgroundColor = count === 1 ? "red" : '#FFFFFF';
+   target.dataset.count = count === 1 ? 0 : 1;
+}
+	</script>
+	
+</head>
+<body>
+
+
+<?php foreach ($users as $user) { ?>
+<?= $user->Name; ?> <?= $user->Date; ?> <br />
+
+<?= $user->Comment; ?>
+<div id="display">
+<p></p></div>
+<div id="input_comment" class="input"> 
+					<input type="text" placeholder= 
+						"Comment" id="add_comment" name="comment" required> 
+						
+  
+						<!--<i id="myDIV" onclick="myFunction(this)" class="fa fa-thumbs-up" ></i>-->
+						
+						
+      <input type="button" id="button" value="LIKE" onclick="setColor(event, 'button', '#101010')"; data-count="1" />
+
+	  
+    
+						
+						
+						<input id="submit" type="submit"
+						name="submit" value="submit"
+						onclick="on_submit()"> 
+				</div>
+				
+				
+<script>
+
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
+function on_submit() {
+	//var titleInput  = document.getElementById("add_comment");
+	
+	//display.innerHTML = "Reply Comment " + titleInput + "<br/>";
+ 
+ var x = document.getElementById("add_comment").value;
+  document.getElementById("display").innerHTML = x;
+}
+</script>
+
+
+
+
+				
+ <?php } ?>
+</body>
+</html>
